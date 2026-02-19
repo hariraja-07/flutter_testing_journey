48d8aef8c93fd62d6bb065082d9257d34c8fd799
hariraja-07 <hariraja1976@gmail.com>
2026-02-19 23:12:54 +0530

    test: add initial Counter unit test

---
+++ test/counter_test.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing_journey/counter.dart';
+
+void main(){
+  // given when then 
+  test(
+    'given Counter class when it is instantiated then value of count should be 0',
+    () {
+      //Arrange - setup
+      final Counter counter = Counter();
+
+      //Act - execute 
+      final value = counter.count; 
+
+      //Assert - verify
+      expect(value,0);
+    }
+  );
+}
