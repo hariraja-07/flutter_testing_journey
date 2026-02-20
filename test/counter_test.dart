21c0122be445b63616deb972c8e7c3fbebc19c1e
hariraja-07 <hariraja1976@gmail.com>
2026-02-20 23:55:48 +0530

    refactor: group Counter tests for better Organization

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -2,34 +2,38 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_testing_journey/counter.dart';
 
 void main(){
+
+  group("Counter Class - ", () {
+
   // given when then 
-  test(
-    'given Counter class when it is instantiated then value of count should be 0',
-    () {
-      //Arrange - setup
-      final Counter counter = Counter();
+    test(
+      'given Counter class when it is instantiated then value of count should be 0',
+      () {
+        //Arrange - setup
+        final Counter counter = Counter();
 
-      //Act - execute 
-      final value = counter.count; 
+        //Act - execute 
+        final value = counter.count; 
 
-      //Assert - verify
-      expect(value,0);
-    }
-  );
+        //Assert - verify
+        expect(value,0);
+      }
+    );
 
-  // given when then
-  test(
-    'given counter at 0 when increment is called once then count should be 1',
-    (){
-      //Arrange - setup 
-      final Counter counter = Counter();
+    // given when then
+    test(
+      'given counter at 0 when increment is called once then count should be 1',
+      (){
+        //Arrange - setup 
+        final Counter counter = Counter();
 
-      //Act - execute 
-      counter.increment();
-      final value = counter.count;
+        //Act - execute 
+        counter.increment();
+        final value = counter.count;
 
-      //Assert - verify 
-      expect(value, 1);
-    }
-  );
+        //Assert - verify 
+        expect(value, 1);
+      }
+    );
+  });
 }
